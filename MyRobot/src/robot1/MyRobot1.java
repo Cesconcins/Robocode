575e40cbb90633d06eb6e0558f2530d74e0a0f82
Cesconcins <cesco.oncins@gmail.com>
2023-10-10 11:44:15 +0200

    esquelet codi

--- MyRobot/src/robot1/MyRobot1.java
+++ MyRobot/src/robot1/MyRobot1.java
@@ -11,26 +11,31 @@ import robocode.*;
 public class MyRobot1 extends TeamRobot {
 
     @Override
-
     public void run() {
-
-        setAdjustGunForRobotTurn(true);
-        setAdjustRadarForGunTurn(true);
-
-        while (true) {
-            setAhead(getBattleFieldWidth() * 0.3);
-            setTurnLeft(90);
-            setTurnRadarRight(90);
-            execute();
-            
-            System.out.println(getHeading()); //mirar en terminal de batalla
-            //SystemDebbugging();
-
-        }
-    }
-    
-    public void onPaint(Graphics2D g){
-        g.drawLine(0,0,(int)this.getX(),(int)this.getY());
+        gestionaRadar();
+        gestionaGun();
+        gestionaMoviment();
+        updateState();
+        finish();
     }
 
+    private void gestionaRadar() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void gestionaGun() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void gestionaMoviment() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void updateState() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void finish() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
 }
