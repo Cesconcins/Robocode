575937cbba46126da5dc4a5fdab55faa639aa6e7
Mariona Farré Tapias <marionafarretapias@gmail.com>
2023-10-15 14:23:14 +0200

    exercici2_droids i lider

--- MyRobot/src/robot1/MyRobot1.java
+++ MyRobot/src/robot1/MyRobot1.java
@@ -8,20 +8,41 @@ import robocode.*;
  *
  * @author usuario
  */
-public class MyRobot1 extends TeamRobot {
+public class MyRobot1 extends AdvancedRobot {
 
     @Override
     public void run() {
+        turnLeft(getHeading());
+        while(true) {
+            setTurnRight(10000);
+            setTurnGunRight(90);
+            setAhead(2000);
+            execute();
+        }
+               /*
         gestionaRadar();
         gestionaGun();
         gestionaMoviment();
         updateState();
         finish();
+        */
     }
 
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent e) {
+    fire(1);
+    }
+ 
+    @Override
+    public void onHitByBullet(HitByBulletEvent e) {
+    turnLeft(180);
+    } 
+    
     private void gestionaRadar() {
     }
 
+    
     private void gestionaGun() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
